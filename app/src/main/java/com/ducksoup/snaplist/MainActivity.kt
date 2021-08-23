fd16905f4200d285cb4a4e9753528b4ca911577a
Jari Haavisto <jari.haavisto@gmail.com>
2021-08-23 22:43:18 +0300

    initialized project

---
+++ app/src/main/java/com/ducksoup/snaplist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ducksoup.snaplist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
